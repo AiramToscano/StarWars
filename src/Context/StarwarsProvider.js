e0ab679bcfd21e499730c3f3518ebaf104320d97
Airam Toscano <airamlobato@gmail.com>
2022-02-24 20:40:18 -0400

    exercicio 3 finalizado

--- src/Context/StarwarsProvider.js
+++ src/Context/StarwarsProvider.js
@@ -6,6 +6,9 @@ import fetchAPI from '../services/fetchAPI';
 function StarwarsProvider({ children }) {
   const [nameStarwars, setnameStarwars] = useState([]);
   const [filterByName, setfilterByName] = useState('');
+  const [filterByColuna, setfilterByColuna] = useState('population');
+  const [filterByComp, setfilterByComp] = useState('maior que');
+  const [filterByValue, setfilterByValue] = useState(0);
 
   // const [filterArrayName, setfilterArrayName] = useState([]);
 
@@ -29,11 +32,114 @@ function StarwarsProvider({ children }) {
     setnameStarwars(filterNameArray);
   };
 
+  const filterColuna = (e) => {
+    setfilterByColuna(e);
+  };
+
+  const filterComp = (e) => {
+    setfilterByComp(e);
+  };
+
+  const filterValue = (e) => {
+    setfilterByValue(e);
+  };
+
+  const filterRotationButom = (number, comparacao) => {
+    // const numberInt = parseFloat(number);
+    if (comparacao === 'menor que') {
+      const buttom = nameStarwars.filter((e) => e.rotation_period < number);
+      // setnameStarwars(buttom);
+      console.log(buttom);
+    }
+    if (comparacao === 'igual a') {
+      const buttom = nameStarwars.filter((e) => e.rotation_period === number);
+      // setnameStarwars(buttom);
+      console.log(buttom);
+    }
+    if (comparacao === 'maior que') {
+      const buttom = nameStarwars.filter((e) => e.rotation_period >= number);
+      console.log(buttom);
+    }
+  };
+
+  const filterPopulationButom = (number, comparacao) => {
+    const numberInt = parseFloat(number);
+    if (comparacao === 'menor que') {
+      const buttom = nameStarwars.filter((e) => e.population < numberInt);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'igual a') {
+      const buttom = nameStarwars.filter((e) => e.population === number);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'maior que') {
+      const buttom = nameStarwars.filter((e) => e.population > numberInt);
+      setnameStarwars(buttom);
+    }
+  };
+
+  const filterOrbitalButom = (number, comparacao) => {
+    const numberInt = parseFloat(number);
+    if (comparacao === 'menor que') {
+      const buttom = nameStarwars.filter((e) => e.orbital_period < numberInt);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'igual a') {
+      const buttom = nameStarwars.filter((e) => e.orbital_period === numberInt);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'maior que') {
+      const buttom = nameStarwars.filter((e) => e.orbital_period > numberInt);
+      setnameStarwars(buttom);
+    }
+  };
+  const filterDiameterButom = (number, comparacao) => {
+    const numberInt = parseFloat(number);
+    if (comparacao === 'menor que') {
+      const buttom = nameStarwars.filter((e) => e.diameter < numberInt);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'igual a') {
+      const buttom = nameStarwars.filter((e) => e.diameter === numberInt);
+      setnameStarwars(buttom);
+    }
+    if (comparacao === 'maior que') {
+      const buttom = nameStarwars.filter((e) => e.diameter > numberInt);
+      setnameStarwars(buttom);
+    }
+  };
+  const filterSurfaceButom = (number, comparacao) => {
+    const numberInt = parseFloat(number);
+    if (comparacao === 'menor que') {
+      const buttom1 = nameStarwars.filter((e) => e.surface_water < numberInt);
+      setnameStarwars(buttom1);
+    }
+    if (comparacao === 'igual a') {
+      const buttom2 = nameStarwars.filter((e) => e.surface_water === numberInt);
+      setnameStarwars(buttom2);
+    }
+    if (comparacao === 'maior que') {
+      const buttom3 = nameStarwars.filter((e) => e.surface_water > numberInt);
+      setnameStarwars(buttom3);
+    }
+  };
+
   const Provider = {
     nameStarwars,
     getApi,
     filterName,
     filterByName,
+    filterByColuna,
+    filterByComp,
+    filterByValue,
+    filterColuna,
+    filterComp,
+    filterValue,
+    filterRotationButom,
+    filterPopulationButom,
+    filterDiameterButom,
+    filterOrbitalButom,
+    filterSurfaceButom,
   };
 
   return (
--- src/pages/StarWars.js
+++ src/pages/StarWars.js
@@ -3,26 +3,86 @@ import StarwarsContext from '../Context/StarwarsContext';
 
 function StarWars() {
   const { Provider } = useContext(StarwarsContext);
-  const { getApi, nameStarwars, filterByName, filterName } = Provider;
+  const { getApi, nameStarwars, filterByName, filterName,
+    filterByColuna, filterByComp, filterByValue,
+    filterColuna, filterComp, filterValue,
+    filterRotationButom, filterPopulationButom, filterOrbitalButom,
+    filterDiameterButom, filterSurfaceButom } = Provider;
 
   useEffect(() => {
     getApi();
   }, []);
 
+  const buttomfilter = () => {
+    if (filterByColuna === 'rotation_period') {
+      filterRotationButom(filterByValue, filterByComp);
+    }
+    if (filterByColuna === 'diameter') {
+      filterDiameterButom(filterByValue, filterByComp);
+    }
+    if (filterByColuna === 'population') {
+      filterPopulationButom(filterByValue, filterByComp);
+    }
+    if (filterByColuna === 'orbital_period') {
+      filterOrbitalButom(filterByValue, filterByComp);
+    }
+    if (filterByColuna === 'surface_water') {
+      filterSurfaceButom(filterByValue, filterByComp);
+    }
+  };
   return (
     <div>
       <header>
         <form>
-          <label htmlFor="teste">
+          <input
+            onChange={ (e) => filterName(e.target.value) }
+            name="filterByName"
+            id="teste"
+            value={ filterByName }
+            data-testid="name-filter"
+          />
+          <select
+            onChange={ (e) => filterColuna(e.target.value) }
+            value={ filterByColuna }
+            name="filterByColuna"
+            data-testid="column-filter"
+          >
+            {/* <option value="">Escolha</option> */}
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+          <select
+            onChange={ (e) => filterComp(e.target.value) }
+            value={ filterByComp }
+            name="filterByComp"
+            data-testid="comparison-filter"
+          >
+            {/* <option value="">Escolha</option> */}
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+          <label htmlFor="teste1">
             <input
-              onChange={ (e) => filterName(e.target.value) }
-              name="filterByName"
-              id="teste"
-              value={ filterByName }
-              data-testid="name-filter"
+              onChange={ (e) => filterValue(e.target.value) }
+              name="filterByValue"
+              id="teste1"
+              type="number"
+              value={ filterByValue }
+              data-testid="value-filter"
             />
           </label>
         </form>
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => buttomfilter() }
+        >
+          filtro
+        </button>
       </header>
       <table>
         <thead>
